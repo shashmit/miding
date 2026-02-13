eef85aab2e9ddfa9ede5dbc5b9682f4d8bbd9df5
Shashmit kumar <shaashmit@gmail.com>
2026-02-14 01:20:14 +0530

    Initial Commit

---
+++ miding/midingApp.swift
@@ -0,0 +1,17 @@
+//
+//  midingApp.swift
+//  miding
+//
+//  Created by Shashmit on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct midingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
